8e96b677a5b5588c291ad3fcba0cadbd1fea9ac3
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-12-26 10:54:18 -0800

    progress

--- backupData.py
+++ backupData.py
@@ -22,16 +22,50 @@ except:
     print("Connection Unsuccessful")
     sys.exit(1)
 
+
 conB = sqlite3.connect("backupMessages.db")
 curB = conB.cursor()
 
-curB.execute("""CREATE TABLE messages (
+curB.execute("""CREATE TABLE IF NOT EXISTS messages (
     message_id INTEGER,
     chat_id INTEGER,
     handle_id INTEGER,
     phone_number TEXT,
-    display_name INTEGER,
+    display_name TEXT,
     is_from_me INTEGER,
     date TEXT,
     text TEXT
 );""")
+
+
+curD.execute("""CREATE TABLE temp1 AS
+    SELECT ROWID AS message_id, chat_id, handle_id, is_from_me, date, text
+    FROM message
+    INNER JOIN chat_message_join
+        ON message.ROWID = chat_message_join.message_id
+;""")
+curD.execute("""CREATE TABLE temp2 AS
+    SELECT message_id, chat_id, handle_id, id AS phone_number, is_from_me, date, text
+    FROM temp1
+    INNER JOIN handle
+        ON temp1.handle_id = handle.ROWID
+;""")
+curD.execute("""CREATE TABLE temp3 AS
+    SELECT message_id, chat_id, handle_id, phone_number, display_name, is_from_me, date, text
+    FROM temp2
+    INNER JOIN chat
+        ON temp2.chat_id = chat.ROWID
+;""")
+
+
+curD.execute("SELECT * FROM temp3")
+data = curD.fetchall()
+
+fields = ','.join('?' for desc in curD.description)
+curB.executemany("INSERT INTO messages values ({})".format(fields), data)
+conB.commit()
+
+curD.execute("DROP TABLE temp1")
+curD.execute("DROP TABLE temp2")
+curD.execute("DROP TABLE temp3")
+
