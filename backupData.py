ee073710da146ae1311c0265c15c30bc5bb0c73f
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-12-26 18:11:32 -0800

    finished database storage, no picuture support yet

--- backupData.py
+++ backupData.py
@@ -15,7 +15,7 @@ def convert_datetime(sec):
 
 
 # CONNECT TO iOS BACKUP FILE
-conD = sqlite3.connect("TEST-BACKUP/3d/3d0d7e5fb2ce288813306e4d4636395e047a3d28")
+conD = sqlite3.connect("BACKUP/3d/3d0d7e5fb2ce288813306e4d4636395e047a3d28")
 curD = conD.cursor()
 try:
     resD = curD.execute("SELECT ROWID FROM message")
@@ -39,28 +39,31 @@ except:
 # MAKE TEMP TABLES
 conD.create_function("CONVERTDATETIME", 1, convert_datetime)
 try:
-    curD.execute("""CREATE TABLE temp1 AS
+    curD.execute("""CREATE TABLE tempX1 AS
         SELECT ROWID AS message_id, chat_id, handle_id, is_from_me, date, CONVERTDATETIME(date) AS dateF, text
         FROM message
         INNER JOIN chat_message_join
             ON message.ROWID = chat_message_join.message_id
     ;""")
-    curD.execute("""CREATE TABLE temp2 AS
+    curD.execute("""CREATE TABLE tempX2 AS
         SELECT message_id, chat_id, handle_id, id AS phone_number, is_from_me, date, dateF, text
-        FROM temp1
+        FROM tempX1
         INNER JOIN handle
-            ON temp1.handle_id = handle.ROWID
+            ON tempX1.handle_id = handle.ROWID
     ;""")
-    curD.execute("""CREATE TABLE temp3 AS
+    curD.execute("""CREATE TABLE tempX3 AS
         SELECT message_id, chat_id, handle_id, phone_number, display_name, is_from_me, date, dateF, text
-        FROM temp2
+        FROM tempX2
         INNER JOIN chat
-            ON temp2.chat_id = chat.ROWID
+            ON tempX2.chat_id = chat.ROWID
         ORDER BY date
     ;""")
     print(" --- Created Temp Tables! --- ")
 except:
     print(" !!! Failed to Make Temp Tables !!! ")
+    curD.execute("DROP TABLE IF EXISTS tempX1")
+    curD.execute("DROP TABLE IF EXISTS tempX2")
+    curD.execute("DROP TABLE IF EXISTS tempX3")
     sys.exit(1)
 
 
@@ -69,35 +72,39 @@ conB = sqlite3.connect("backupMessages.db")
 curB = conB.cursor()
 try:
     curB.execute("""CREATE TABLE messages (
-        message_id INTEGER,
-        chat_id INTEGER,
-        handle_id INTEGER,
-        phone_number TEXT,
+        message_id INTEGER NOT NULL,
+        chat_id INTEGER NOT NULL,
+        handle_id INTEGER NOT NULL,
+        phone_number TEXT NOT NULL,
         display_name TEXT,
         is_from_me INTEGER,
         date TEXT,
         dateF TEXT,
-        text TEXT
-);""")
+        text TEXT,
+        PRIMARY KEY (message_id, chat_id, handle_id, phone_number)
+    );""")
     print(" --- New Table Made! --- ")
 except:
     print(" --- Previous Table Found! --- ")
 
 
-# COPY TEMP3 TABLE TO BACKUP TABLE
-curD.execute("SELECT * FROM temp3")
+# ADD TEMP3 TABLE TO BACKUP TABLE
+curD.execute("SELECT * FROM tempX3")
 data = curD.fetchall()
 
 fields = ','.join('?' for desc in curD.description)
-curB.executemany("INSERT INTO messages values ({})".format(fields), data)
+curB.executemany("INSERT OR IGNORE INTO messages values ({})".format(fields), data)
 conB.commit()
 
 # CLEANUP AND CLOSE
-curD.execute("DROP TABLE temp1")
-curD.execute("DROP TABLE temp2")
-curD.execute("DROP TABLE temp3")
+curD.execute("DROP TABLE IF EXISTS tempX1")
+curD.execute("DROP TABLE IF EXISTS tempX2")
+curD.execute("DROP TABLE IF EXISTS tempX3")
 conD.close()
 conB.close()
 
-print(" --- Successfully Backed Up Messages! --- ")
+print(" --- SUCCESSFULLY BACKED UP MESSAGES! --- ")
 print()
+print()
+
+input("Press any key to exit...")
