80f1d4aa2c198d6b50b379738edd8f809345e111
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-12-26 17:12:10 -0800

    progress on datetime format

--- backupData.py
+++ backupData.py
@@ -1,8 +1,19 @@
 import sys
 import sqlite3
+from datetime import datetime
+
 print()
 print(" --- Starting Script --- ")
 
+
+# Convert seconds since "2001-1-1 00:00:00" to "YYYY-MM-DD HH:MM:SS" Format
+def convert_datetime(sec):
+    sec += 978307200
+    date = datetime.fromtimestamp(sec)
+    return date.strftime("%Y-%m-%d")
+
+
+# CONNECT TO iOS BACKUP FILE
 conD = sqlite3.connect("TEST-BACKUP/3d/3d0d7e5fb2ce288813306e4d4636395e047a3d28")
 curD = conD.cursor()
 try:
@@ -24,28 +35,11 @@ except:
     sys.exit(1)
 
 
-conB = sqlite3.connect("backupMessages.db")
-curB = conB.cursor()
-try:
-    curB.execute("""CREATE TABLE messages (
-        message_id INTEGER,
-        chat_id INTEGER,
-        handle_id INTEGER,
-        phone_number TEXT,
-        display_name TEXT,
-        is_from_me INTEGER,
-        date TEXT,
-        dateF TEXT,
-        text TEXT
-);""")
-    print(" --- New Table Made! --- ")
-except:
-    print(" --- Previous Table Found! --- ")
-
-
+# MAKE TEMP TABLES
+conD.create_function("CONVERTDATETIME", 1, convert_datetime)
 try:
     curD.execute("""CREATE TABLE temp1 AS
-        SELECT ROWID AS message_id, chat_id, handle_id, is_from_me, date, DATETIME(date, 'unixepoch') AS dateF, text
+        SELECT ROWID AS message_id, chat_id, handle_id, is_from_me, date, CONVERTDATETIME(date) AS dateF, text
         FROM message
         INNER JOIN chat_message_join
             ON message.ROWID = chat_message_join.message_id
@@ -69,6 +63,27 @@ except:
     sys.exit(1)
 
 
+# CONNECT/MAKE BACKUP TABLE
+conB = sqlite3.connect("backupMessages.db")
+curB = conB.cursor()
+try:
+    curB.execute("""CREATE TABLE messages (
+        message_id INTEGER,
+        chat_id INTEGER,
+        handle_id INTEGER,
+        phone_number TEXT,
+        display_name TEXT,
+        is_from_me INTEGER,
+        date TEXT,
+        dateF TEXT,
+        text TEXT
+);""")
+    print(" --- New Table Made! --- ")
+except:
+    print(" --- Previous Table Found! --- ")
+
+
+# COPY TEMP3 TABLE TO BACKUP TABLE
 curD.execute("SELECT * FROM temp3")
 data = curD.fetchall()
 
@@ -76,10 +91,10 @@ fields = ','.join('?' for desc in curD.description)
 curB.executemany("INSERT INTO messages values ({})".format(fields), data)
 conB.commit()
 
+# CLEANUP AND CLOSE
 curD.execute("DROP TABLE temp1")
 curD.execute("DROP TABLE temp2")
 curD.execute("DROP TABLE temp3")
-
 conD.close()
 conB.close()
 
