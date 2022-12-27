227df12df8ae3580887de4aa98b3b4ae9d83544a
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-12-26 16:21:25 -0800

    almost finished with data backup, no pictures or correct date format

--- backupData.py
+++ backupData.py
@@ -1,62 +1,72 @@
 import sys
 import sqlite3
+print()
+print(" --- Starting Script --- ")
 
 conD = sqlite3.connect("TEST-BACKUP/3d/3d0d7e5fb2ce288813306e4d4636395e047a3d28")
 curD = conD.cursor()
-
 try:
     resD = curD.execute("SELECT ROWID FROM message")
     if resD.fetchone() is None:
-        print("Connection Unsuccessful")
+        print(" !!! Connection Unsuccessful !!! ")
         sys.exit(1)
     resD = curD.execute("SELECT ROWID FROM chat")
     if resD.fetchone() is None:
-        print("Connection Unsuccessful")
+        print(" !!! Connection Unsuccessful !!! ")
         sys.exit(1)
     resD = curD.execute("SELECT ROWID FROM chat_message_join")
     if resD.fetchone() is None:
-        print("Connection Unsuccessful")
+        print(" !!! Connection Unsuccessful !!! ")
         sys.exit(1)
-    print("Connection Successful")
+    print(" --- Connection Successful! --- ")
 except:
-    print("Connection Unsuccessful")
+    print(" !!! Connection Unsuccessful !!! ")
     sys.exit(1)
 
 
 conB = sqlite3.connect("backupMessages.db")
 curB = conB.cursor()
-
-curB.execute("""CREATE TABLE IF NOT EXISTS messages (
-    message_id INTEGER,
-    chat_id INTEGER,
-    handle_id INTEGER,
-    phone_number TEXT,
-    display_name TEXT,
-    is_from_me INTEGER,
-    date TEXT,
-    text TEXT
+try:
+    curB.execute("""CREATE TABLE messages (
+        message_id INTEGER,
+        chat_id INTEGER,
+        handle_id INTEGER,
+        phone_number TEXT,
+        display_name TEXT,
+        is_from_me INTEGER,
+        date TEXT,
+        dateF TEXT,
+        text TEXT
 );""")
+    print(" --- New Table Made! --- ")
+except:
+    print(" --- Previous Table Found! --- ")
 
 
-curD.execute("""CREATE TABLE temp1 AS
-    SELECT ROWID AS message_id, chat_id, handle_id, is_from_me, date, text
-    FROM message
-    INNER JOIN chat_message_join
-        ON message.ROWID = chat_message_join.message_id
-;""")
-curD.execute("""CREATE TABLE temp2 AS
-    SELECT message_id, chat_id, handle_id, id AS phone_number, is_from_me, date, text
-    FROM temp1
-    INNER JOIN handle
-        ON temp1.handle_id = handle.ROWID
-;""")
-curD.execute("""CREATE TABLE temp3 AS
-    SELECT message_id, chat_id, handle_id, phone_number, display_name, is_from_me, date, text
-    FROM temp2
-    INNER JOIN chat
-        ON temp2.chat_id = chat.ROWID
-    ORDER BY date
-;""")
+try:
+    curD.execute("""CREATE TABLE temp1 AS
+        SELECT ROWID AS message_id, chat_id, handle_id, is_from_me, date, DATETIME(date, 'unixepoch') AS dateF, text
+        FROM message
+        INNER JOIN chat_message_join
+            ON message.ROWID = chat_message_join.message_id
+    ;""")
+    curD.execute("""CREATE TABLE temp2 AS
+        SELECT message_id, chat_id, handle_id, id AS phone_number, is_from_me, date, dateF, text
+        FROM temp1
+        INNER JOIN handle
+            ON temp1.handle_id = handle.ROWID
+    ;""")
+    curD.execute("""CREATE TABLE temp3 AS
+        SELECT message_id, chat_id, handle_id, phone_number, display_name, is_from_me, date, dateF, text
+        FROM temp2
+        INNER JOIN chat
+            ON temp2.chat_id = chat.ROWID
+        ORDER BY date
+    ;""")
+    print(" --- Created Temp Tables! --- ")
+except:
+    print(" !!! Failed to Make Temp Tables !!! ")
+    sys.exit(1)
 
 
 curD.execute("SELECT * FROM temp3")
@@ -73,3 +83,5 @@ curD.execute("DROP TABLE temp3")
 conD.close()
 conB.close()
 
+print(" --- Successfully Backed Up Messages! --- ")
+print()
